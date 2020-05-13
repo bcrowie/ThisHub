d478ec4e7821e2e468a5e8de6f65a32d077ff0d1
Brenden Crowie <brendencrowie@gmail.com>
2020-05-13 18:27:13 -0400

    Added routes to contants, cleaned up

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,34 +1,51 @@
-export const PostConst = {
-  Endpoint: {
-    Posts: "/posts",
+export const Constants = {
+  Posts: {
+    Votes: {
+      like: "Liked",
+      dislike: "Disliked",
+      likedDisliked: "Liked and Disliked",
+      dislikedLiked: "Disliked and Liked",
+      removedLike: "Removed Like",
+      removedDislike: "Removed Dislike",
+    },
   },
-  Likes: {
-    like: "Liked",
-    dislike: "Disliked",
-    likedDisliked: "Liked and Disliked",
-    dislikedLiked: "Disliked and Liked",
-    removedLike: "Removed Like",
-    removedDislike: "Removed Dislike",
+  Users: {
+    Storage: {
+      Token: "thishub.token",
+      User: "thishub.user",
+    },
+    LoginDefault: {
+      Email: null,
+      Password: null,
+    },
   },
-};
-
-export const UserConst = {
-  Endpoint: {
-    Auth: `/users/auth`,
-    Login: `/users/login`,
-    Register: `/users/register`,
-    Users: "/users",
-  },
-  Storage: {
-    Token: "thishub.token",
-    User: "thishub.user",
-  },
-  LoginDefault: {
-    Email: null,
-    Password: null,
+  Comments: {
+    deleted: "[Deleted]",
   },
 };
 
-export const CommentConst = {
-  deleted: "[Deleted]",
+export const Routes = {
+  Posts: {
+    posts: "/posts",
+    getPostWithId: (PostId) => {
+      return `/posts/${PostId}`;
+    },
+  },
+  Users: {
+    myAccount: "/users/my-account",
+    auth: `/users/auth`,
+    login: `/users/login`,
+    register: `/users/register`,
+    users: "/users",
+    updateEmail: "/users/my-account/change-email",
+    updatePassword: "/users/my-account/change-password",
+  },
+  Comments: {
+    getComments: (PostId) => {
+      return `/posts/${PostId}/comments`;
+    },
+    getCommentById: (PostId, CommentId) => {
+      return `/posts/${PostId}/comments/${CommentId}`;
+    },
+  },
 };
