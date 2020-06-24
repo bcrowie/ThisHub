a334f79b70907c69fc29bf98148f2f25ac33ca32
Brenden Crowie <brendencrowie@gmail.com>
2020-06-24 12:38:56 -0400

    Port modified

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require("./config/database");
 const models = require("./models");
 const app = express();
 const cors = require("cors");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
